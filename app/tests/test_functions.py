97effa6f18b1b94427ef45ff9b398cfcff24d8c5
EmilSundman <sundman.emil@gmail.com>
2023-06-18 01:29:55 +0200

    Update test_functions.py

--- app/tests/test_functions.py
+++ app/tests/test_functions.py
@@ -9,3 +9,9 @@ def test_square_number():
     Test the square_number function
     """
     assert fn.square_number(input_number_to_square) == input_number_to_square ** 2
+    
+def test_square_number_will_fail():
+    """
+    Test the square_number function
+    """
+    assert fn.square_number(input_number_to_square) == (input_number_to_square + 2) ** 2
