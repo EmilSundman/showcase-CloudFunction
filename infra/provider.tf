3afa5c1336064d38a518743ad899ea260817aadb
EmilSundman <sundman.emil@gmail.com>
2023-06-18 20:43:18 +0200

    Remove credentials

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,12 +1,12 @@
 provider "google" {
-  project     = var.project_id
-  region      = var.region
-  zone        = var.zone
-  credentials = file(var.credentials_file)
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+  #   credentials = file(var.credentials_file)
 }
 provider "google-beta" {
-  project     = var.project_id
-  region      = var.region
-  zone        = var.zone
-  credentials = file(var.credentials_file)
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+  #   credentials = file(var.credentials_file)
 }
